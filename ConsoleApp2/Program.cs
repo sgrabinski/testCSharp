58f063e22aea9cfde44056ea1c3eef263e8050ea
Sebastian Grabiński <sgrabinski@parasoft.com.pl>
2020-09-11 16:32:39 +0200

    test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Console.WriteLine("Project 2 ConsoleApp2");
+            Console.ReadKey();
+        }
+    }
+}
